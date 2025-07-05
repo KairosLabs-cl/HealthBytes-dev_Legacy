b106113ef780f2e9c2e0edfbd61988079bf4b171
Simón Aspée <simonaspee@gmail.com>
2025-07-05 02:49:19 -0400

    Login-Pt.1
    Implementacion de el boton de login , usuario, contraseña, inicio de sesion y creacion de la cuenta.
    
    Instalar antes de iniciar:
    
    npx gluestack-ui add form-control
    npx gluestack-ui add input

--- shop/app/_layout.tsx
+++ shop/app/_layout.tsx
@@ -1,8 +1,9 @@
 import "@/global.css";
 import { Link, Stack } from "expo-router";
 import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { Icon } from "@/components/ui/icon";
-import { ShoppingCart } from "lucide-react-native";
+import { ShoppingCart, User } from "lucide-react-native";
 import { Pressable } from "react-native";
 import { useCart } from "@/store/cartStore";
 import { Text } from "@/components/ui/text";
@@ -22,12 +23,19 @@ export default function RootLayout() {
                                 <Text>{cartItemsNum}</Text>
                             </Pressable>
                         </Link>
+                        ),
+                    headerLeft: () => (
+                        <Link href={"/login"} asChild> 
+                            <Pressable className="flex-row gap-2" style={{ marginLeft: 50 }}>
+                                <Icon as={User} />
+                            </Pressable>
+                        </Link>
                     ),
                 }}
             >
-                <Stack.Screen name="index" options={{title: "Shop"}}/>
-                <Stack.Screen name="product/[id]" options={{title: "Product"}}/>
-                
+                <Stack.Screen name="index" options={{ title: "Shop", headerTitleAlign: "center" }} />
+                <Stack.Screen name="product/[id]" options={{ title: "Product"}} />
+
             </Stack>
             
         </GluestackUIProvider>
