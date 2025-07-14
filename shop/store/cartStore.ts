c97c454a50799c04f30e6951c249741faa51a85d
Guillermo <guillermoserrano.0866@gmail.com>
2025-07-14 15:46:05 -0400

    Se realiza cambios minimos pero importantes; Cambio en el carrito que se agrege cantidad +1 cuando se agregan mas del mismo, se movio un poco el icono para que se vea mas centrado, y ahora el icono del carrito muestra la cantidad de productos que hay en el carrito actualizada segun total diferentes productos y del mismo.

--- shop/store/cartStore.ts
+++ shop/store/cartStore.ts
@@ -1,12 +1,17 @@
+type Product = {
+  id: string | number; // Usa el tipo correcto según tu modelo de producto
+  name: string;
+  price: number;
+};
+
+
+
+
 //Definimos types para el carrito de compras, ya que Zustand no infiere el tipo de estado
 type CartItem = {
-    product: {
-        name: string;
-        price: number;
-
-    };
+    product: Product; // Aquí usas Product, no solo name y price
     quantity: number;
-};
+}
 
 
 type CartState = {
@@ -14,22 +19,32 @@ type CartState = {
     addProduct: (product: any) => void;
     reserCart: () => void;
 }
-
-
-
+// ------------------------------------------------------
 
 
 import { create } from "zustand";
 
-export const useCart = create<CartState>((set) => ({ //Se hace el llamado al type y eso soluciona el problema
-    items: [],
+export const useCart = create<CartState>((set) => ({
+  items: [],
 
-    addProduct: (product: any) => 
-        // TODO: if already in cart, increase quantity, else add new item
-        set((state) => ({
-             items: [...state.items, {product, quantity: 1}],
-        })),
+  addProduct: (product) =>
+    set((state) => {
+      const existingItem = state.items.find(
+        (item) => item.product.id === product.id
+      );
+      if (existingItem) {
+        return {
+          items: state.items.map((item) =>
+            item.product.id === product.id
+              ? { ...item, quantity: item.quantity + 1 }
+              : item
+          ),
+        };
+      }
+      return {
+        items: [...state.items, { product, quantity: 1 }],
+      };
+    }),
 
-    reserCart: () => set({ items: []})
+  reserCart: () => set({ items: [] }),
 }));
-
