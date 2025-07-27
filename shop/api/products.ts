5134b302c1fddea59b24aa674ab145c9fc4469c0
Benjamin Zamora <windb3nja@gmail.com>
2025-07-27 17:17:19 -0400

    Se agrega conexiones: - Back end para el registro de usuarios de manera permanente - inicio y registro de seccion de usuarios
    autenticacion y autorizacion

--- shop/api/products.ts
+++ shop/api/products.ts
@@ -1,8 +1,6 @@
 const API_URL = process.env.EXPO_PUBLIC_API_URL;
 
 export async function listProducts() {
-  console.log('API_URL:', API_URL);
-  console.log('Full URL:', `${API_URL}/products`);
   const res = await fetch(`${API_URL}/products`);
   const data = await res.json();
   if (!res.ok) {
