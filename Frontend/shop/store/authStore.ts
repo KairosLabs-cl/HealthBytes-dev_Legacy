0ba02d0d9263328576a752b6e8b93fde7f09b460
Guillermo <guillermoserrano.0866@gmail.com>
2025-09-07 16:46:03 -0300

    Se agregar barra de recientes para pagina principal funcional, se crea su ts en store, su componente en carpeta components, y se agrega en el app

--- Frontend/shop/store/authStore.ts
+++ Frontend/shop/store/authStore.ts
@@ -2,13 +2,15 @@ import { create } from "zustand";
 import { persist, createJSONStorage } from "zustand/middleware";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
+
 export const useAuth = create(
   persist((set) => ({
     user: null,
     token: null,
+    setToken: (token) => set({ token }),
+    logout: () => set({ token: null }),
 
     setUser: (user) => set({ user }),
-    setToken: (token) => set({ token }),
   }), {
     name: "auth-store",
     storage: createJSONStorage(() => AsyncStorage),
